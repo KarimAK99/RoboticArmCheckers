c67cde620293e5c4cfeb926718ff36e24308d7e4
Olive <35248380+OliveIsLazy@users.noreply.github.com>
2020-01-17 16:01:44 +0100

    python main barebones
    edit as needed, guidelines have been added at the top of the code

---
+++ src/pyMain.py
@@ -0,0 +1,35 @@
+# Python main class
+# =========================================
+# NOTE(s)
+#	- to import a python file, write "import [name]" without the .py extension
+#	- running a script can be done only once
+#	- you can run a method multiple times via "[filename].[methodcall]()"
+#	- files must be in the same directory as this main class (package handling hasn't been implemented)
+
+
+# Initial set-up code.
+import time
+
+# Import the files to be called.
+import BoardReturnLoop
+import motion_vision
+import sercom
+
+# Initiate loop.
+print("Entering python main loop-- ")
+while 1:
+	# Detect the board positions
+	BoardReturnLoop.main()
+
+	# Detect if there is obstruction
+	motion_vision.motion_color()
+
+	# Begin moving
+	sercom.main()
+
+	# Repeat loop...
+	print("Python loop complete, will repeat after a second delay!-- ")
+	time.sleep(1)
+
+# Initiate termination.
+# this is empty because we should never reach this part...
